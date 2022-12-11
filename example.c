3a12785e2091efc6a03d9dbfadf32353c6410ef6
Adam Brutsaert <adam.brutsaert@epitech.eu>
2022-12-11 03:20:52 +0100

    [+] Added example.c

---
+++ example.c
@@ -0,0 +1,20 @@
+#include "profiler.h"
+
+int main(void)
+{
+    PROFILE_FUNC();
+
+        PROFILE_BEGIN("FirstPart");
+            for (uint64_t i = 0; i < 3200000000; i++)
+                i++;
+        PROFILE_END();
+
+        PROFILE_BEGIN("SecondPart");
+            for (uint64_t i = 0; i < 320000000; i++)
+                i += 2;
+        PROFILE_END();
+
+    PROFILE_END();
+
+    return 0;
+}
